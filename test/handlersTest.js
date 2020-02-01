2cce2fc786c4d96d66cdf069d473b7c3226feb54
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-02-01 14:40:14 +0530

    added tests for homepage

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -2,14 +2,37 @@ const request = require('supertest');
 const {app} = require('../lib/handlers');
 
 const statusCodes = {
-  OK: 200
+  OK: 200,
+  NOT_FOUND: 404
 };
 
-describe('Home Page', () => {
-  it('should get the home.html for "/" path', (done) => {
-    request(app.handleRequest.bind(app))
-      .get('/')
-      .expect(statusCodes.OK)
-      .expect('Content-Type', 'text/html', done);
+describe('GET', () => {
+  describe('Home Page', () => {
+    it('should get the home.html for "/" path', done => {
+      request(app.handleRequest.bind(app))
+        .get('/')
+        .expect(statusCodes.OK)
+        .expect('Content-Length', '518')
+        .expect('Content-Type', 'text/html')
+        .expect(/<title>TODO<\/title>/, done);
+    });
+
+    it('should get the index.css for "/css/index.css" path', done => {
+      request(app.handleRequest.bind(app))
+        .get('/css/index.css')
+        .expect(statusCodes.OK)
+        .expect('Content-Type', 'text/css')
+        .expect('Content-Length', '954')
+        .expect(/body {/, done);
+    });
+
+    it('should get the home.js for "/js/home.js" path', done => {
+      request(app.handleRequest.bind(app))
+        .get('/js/home.js')
+        .expect(statusCodes.OK)
+        .expect('Content-Type', 'application/javascript')
+        .expect('Content-Length', '979')
+        .expect(/window.onload = main/, done);
+    });
   });
 });
