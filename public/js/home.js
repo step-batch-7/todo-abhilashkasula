ec88eaa06f564037a74624b0e1bba92ba9f57ee2
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-02-04 13:17:43 +0530

    refactored the code in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -2,81 +2,46 @@ const statusCodes = {
   OK: 200
 };
 
-const getTextBox = function(classes, attributes) {
-  const titleBox = document.createElement('input');
-  const keys = Object.keys(attributes);
-  titleBox.setAttribute('type', 'text');
-  keys.forEach(key => titleBox.setAttribute(key, attributes[key]));
-  classes.forEach(cssClass => titleBox.classList.add(cssClass));
-  return titleBox;
-};
-
-const sendHttpGet = (url, callback) => {
+const sendXHR = (method, url, message, callback) => {
   const req = new XMLHttpRequest();
   req.onload = function() {
     if (this.status === statusCodes.OK) {
       callback(this.responseText);
     }
   };
-  req.open('GET', url);
-  req.send();
-};
-
-const postHttpMsg = function(url, callback, message) {
-  const req = new XMLHttpRequest();
-  req.onload = function() {
-    if (this.status === statusCodes.OK) {
-      callback(this.responseText);
-    }
-  };
-  req.open('POST', url);
-  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  req.open(method, url);
   req.send(message);
 };
 
 const addTask = function() {
   const inputBox = event.target.previousElementSibling;
-  postHttpMsg('/addTask', generateTasks, `title=${inputBox.value}`);
+  sendXHR('POST', '/addTask', `title=${inputBox.value}`, showTasks);
   inputBox.value = '';
-};
-
-const generateTasks = function(text) {
-  const todoLists = document.querySelector('.todo-lists');
-  const tasksJSON = JSON.parse(text);
-  const tasks = tasksJSON.map(createTasks);
-  todoLists.innerHTML = '';
-  tasks.forEach(task => todoLists.appendChild(task));
 };
 
 const deleteTask = function() {
   const [, , task] = event.path;
   const taskId = task.id;
-  postHttpMsg('/removeTask', generateTasks, `id=${taskId}`);
-};
-
-const createImage = function(src, classes, eventListener) {
-  const img = document.createElement('img');
-  img.setAttribute('src', src);
-  classes.forEach(cssClass => img.classList.add(cssClass));
-  img.addEventListener('click', eventListener);
-  return img;
-};
-
-const createTaskHeader = function(task) {
-  const taskHeader = document.createElement('div');
-  const taskTitle = document.createElement('h3');
-  const img = createImage('svg/remove.svg', ['svg'], deleteTask);
-  taskTitle.classList.add('task-title');
-  taskTitle.textContent = task.title;
-  taskHeader.classList.add('task-headline');
-  taskHeader.appendChild(taskTitle);
-  taskHeader.appendChild(img);
-  return taskHeader;
+  sendXHR('POST', '/removeTask', `id=${taskId}`, showTasks);
 };
 
 const addSubTask = function() {
   const text = event.target.previousElementSibling.value;
 
+};
+
+const convertHtmlTextToNode = function(html) {
+  const temp = document.createElement('div');
+  temp.innerHTML = html;
+  return temp.firstChild;
+};
+
+const createTaskHeader = function(taskTitle) {
+  const html = `<div class="task-headline">
+    <h3 class="task-title">${taskTitle}</h3>
+    <img src="svg/remove.svg" class="svg" onclick="deleteTask()">
+  </div>`;
+  return convertHtmlTextToNode(html);
 };
 
 const generateSubtasks = function(subTasksHtml, subTask) {
@@ -91,47 +56,41 @@ const generateSubtasks = function(subTasksHtml, subTask) {
   return subTasksHtml + subTaskHtml;
 };
 
-const appendChildHtml = function(element, html) {
-  const temp = document.createElement('div');
-  temp.innerHTML = html;
-  temp.firstChild && element.appendChild(temp.firstChild);
+const generateSubTasksContainer = function(tasks) {
+  const subTasks = tasks.reduce(generateSubtasks, '');
+  const html = `<div class="subtasks">${subTasks}</div>`;
+  return convertHtmlTextToNode(html);
 };
 
-const createSubTasks = function(task) {
-  const subTasks = document.createElement('div');
-  subTasks.classList.add('subtasks');
-  appendChildHtml(subTasks, task.subTasks.reduce(generateSubtasks, ''));
-  return subTasks;
+const generateSubTasksAdder = function() {
+  const placeholder = 'Add your sub task here';
+  const html = `<div class="sub-tasks-adder">
+  <input type="text" class="sub-task-box box" placeholder="${placeholder}">
+  <img src="svg/plus.svg" class="svg sub-task-svg" onclick="addSubTask()">
+  </div>`;
+  return convertHtmlTextToNode(html);
 };
 
-const createSubTasksAdder = function() {
-  const attributes = {placeholder: 'Add your subTask Here'};
-  const textBox = getTextBox(['sub-task-box', 'box'], attributes);
-  const img = createImage('svg/plus.svg', ['svg', 'sub-task-svg'], addSubTask);
-  const subTasksAdder = document.createElement('div');
-  subTasksAdder.classList.add('sub-tasks-adder');
-  subTasksAdder.appendChild(textBox);
-  subTasksAdder.appendChild(img);
-  return subTasksAdder;
-};
-
-const createTasks = function(task) {
+const generateTask = function(task) {
   const taskContainer = document.createElement('div');
-  const taskHeader = createTaskHeader(task);
   taskContainer.id = task.id;
   taskContainer.classList.add('task-container');
-  taskContainer.appendChild(taskHeader);
-  taskContainer.appendChild(createSubTasksAdder());
-  taskContainer.appendChild(createSubTasks(task));
+  taskContainer.appendChild(createTaskHeader(task.title));
+  taskContainer.appendChild(generateSubTasksAdder());
+  taskContainer.appendChild(generateSubTasksContainer(task.subTasks));
   return taskContainer;
 };
 
+const showTasks = function(text) {
+  const todoLists = document.querySelector('.todo-lists');
+  const tasksJSON = JSON.parse(text);
+  const tasks = tasksJSON.map(generateTask);
+  todoLists.innerHTML = '';
+  tasks.forEach(task => todoLists.appendChild(task));
+};
+
 const loadTasks = function() {
-  sendHttpGet('/tasks', generateTasks);
+  sendXHR('GET', '/tasks', '', showTasks);
 };
 
-const main = function() {
-  loadTasks();
-};
-
-window.onload = main;
+window.onload = loadTasks;
